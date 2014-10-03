d7f6adc74ebf087c1449e9c614245e60c4f5f61a
Emil Lunnergård <emil.lunnergard@gmail.com>
2014-10-03 07:41:33 +0200

    fix

--- src/Singleton.php
+++ src/Singleton.php
@@ -6,8 +6,8 @@ abstract class Singleton
 {
   protected static $instance;
 
-  private function __clone()     { } 
-  private function __construct() { } 
+  private function __clone()     { }
+  private function __construct() { }
   private function __wakeup()    { }
 
   final public static function get() {
