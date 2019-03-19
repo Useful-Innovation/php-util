93fc66410edb2eab069a64d59b913d6d555c9a7a
Peter Nilsson <peter@gobrave.se>
2019-03-19 13:55:47 +0100

    feat: add slice method to Collection

--- src/Collection.php
+++ src/Collection.php
@@ -4,88 +4,101 @@ namespace GoBrave\Util;
 
 class Collection implements \ArrayAccess, \IteratorAggregate
 {
-  private $items;
-  private $key;
-  public function __construct(array $items = [], $key = 'ID') {
-    $this->items = $items;
-    $this->key   = $key;
-  }
-
-  //
-  //  ArrayAccess
-  //
-  public function offsetGet($offset) {
-    if(!$this->offsetExists($offset)) {
-      throw new \OutOfRangeException("Undefined offset '" . $offset . "'");
+    private $items;
+    private $key;
+    public function __construct(array $items = [], $key = 'ID')
+    {
+        $this->items = $items;
+        $this->key = $key;
     }
-    return $this->get($offset);
-  }
 
-  public function offsetSet($offset, $item) {
-    return $this->set($offset, $item);
-  }
-
-  public function offsetUnset($offset) {
-    if($this->offsetExists($offset)) {
-      $pos = $this->posFor($offset, $this->key);
-      unset($this->items[$pos]);
+    //
+    //  ArrayAccess
+    //
+    public function offsetGet($offset)
+    {
+        if (!$this->offsetExists($offset)) {
+            throw new \OutOfRangeException("Undefined offset '" . $offset . "'");
+        }
+        return $this->get($offset);
     }
-  }
 
-  public function offsetExists($offset) {
-    return $this->posFor($offset, $this->key) !== false;
-  }
-
-  //
-  //  IteratorAggregate
-  //
-  public function getIterator() {
-    return new \ArrayIterator($this->toArray());
-  }
-
-  //
-  //  Regular API
-  //
-  public function get($item_identifier) {
-    return $this->items[$this->posFor($item_identifier, $this->key)];
-  }
-
-  public function set($item_identifier, $item) {
-    $pos = $this->posFor($item_identifier, $this->key);
-    if($pos !== false) {
-      $this->items[$pos] = $item;
-    } else {
-      $this->items[] = $item;
+    public function offsetSet($offset, $item)
+    {
+        return $this->set($offset, $item);
     }
-  }
 
-  public function toArray() {
-    return $this->items;
-  }
-
-  public function length() {
-    return count($this->items);
-  }
-
-
-
-
-  //
-  //  Helpers
-  //
-  private function posFor($item_identifier, $key) {
-    foreach($this->items as $pos => $item) {
-      if(is_array($item) AND isset($item[$key]) AND $item[$key] == $item_identifier) {
-        return $pos;
-      }
-      if(method_exists($item, $key) AND $item->{$key}() == $item_identifier) {
-        return $pos;
-      }
-      if($item->{$key} == $item_identifier) {
-        return $pos;
-        break;
-      }
+    public function offsetUnset($offset)
+    {
+        if ($this->offsetExists($offset)) {
+            $pos = $this->posFor($offset, $this->key);
+            unset($this->items[$pos]);
+        }
     }
-    return false;
-  }
+
+    public function offsetExists($offset)
+    {
+        return $this->posFor($offset, $this->key) !== false;
+    }
+
+    //
+    //  IteratorAggregate
+    //
+    public function getIterator()
+    {
+        return new \ArrayIterator($this->toArray());
+    }
+
+    //
+    //  Regular API
+    //
+    public function get($item_identifier)
+    {
+        return $this->items[$this->posFor($item_identifier, $this->key)];
+    }
+
+    public function slice($offset, $length = null)
+    {
+        return new static(array_slice($this->items, $offset, $length, true));
+    }
+
+    public function set($item_identifier, $item)
+    {
+        $pos = $this->posFor($item_identifier, $this->key);
+        if ($pos !== false) {
+            $this->items[$pos] = $item;
+        } else {
+            $this->items[] = $item;
+        }
+    }
+
+    public function toArray()
+    {
+        return $this->items;
+    }
+
+    public function length()
+    {
+        return count($this->items);
+    }
+
+    //
+    //  Helpers
+    //
+    private function posFor($item_identifier, $key)
+    {
+        foreach ($this->items as $pos => $item) {
+            if (is_array($item) and isset($item[$key]) and $item[$key] == $item_identifier) {
+                return $pos;
+            }
+            if (method_exists($item, $key) and $item->{$key}() == $item_identifier) {
+                return $pos;
+            }
+            if ($item->{$key} == $item_identifier) {
+                return $pos;
+                break;
+            }
+        }
+        return false;
+    }
 }
