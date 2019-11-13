deb65becd6e7c679609956a0d4b8764282f77f65
Peter Nilsson <peter@gobrave.se>
2019-11-13 11:47:00 +0100

    fix: replace deprecated create_function

--- src/CaseConverter.php
+++ src/CaseConverter.php
@@ -5,31 +5,33 @@ namespace GoBrave\Util;
 class CaseConverter
 {
 
-  //
-  //  Gives
-  //    camel_case -> camelCase
-  //    camel_case -> CamelCase (with second param as true)
-  //
-  public function snakeToCamel($string, $upper_case_first_letter = false) {
-    $string = str_replace(' ', '', ucwords(str_replace('_', ' ', $string)));
-    $string = strtolower(substr($string,0,1)) . substr($string,1);
-    if($upper_case_first_letter) {
-      $string = ucfirst($string);
+    //
+    //  Gives
+    //    camel_case -> camelCase
+    //    camel_case -> CamelCase (with second param as true)
+    //
+    public function snakeToCamel($string, $upper_case_first_letter = false)
+    {
+        $string = str_replace(' ', '', ucwords(str_replace('_', ' ', $string)));
+        $string = strtolower(substr($string, 0, 1)) . substr($string, 1);
+        if ($upper_case_first_letter) {
+            $string = ucfirst($string);
+        }
+        return $string;
     }
-    return $string;
-  }
 
-  //
-  //  Gives
-  //    camelCase -> camel_case
-  //    CamelCase -> camel_case
-  //
-  public function camelToSnake($string) {
-    $string = strtolower(substr($string, 0, 1)) . substr($string, 1);
-    return preg_replace_callback(
-      '/[A-Z]/',
-      create_function('$match', 'return "_" . strtolower($match[0]);'),
-      $string
-    );
-  }
+    //
+    //  Gives
+    //    camelCase -> camel_case
+    //    CamelCase -> camel_case
+    //
+    public function camelToSnake($string)
+    {
+        $string = strtolower(substr($string, 0, 1)) . substr($string, 1);
+        return preg_replace_callback(
+            '/[A-Z]/',
+            function ($match) {return "_" . strtolower($match[0]);},
+            $string
+        );
+    }
 }
